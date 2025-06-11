c91a896714e96eb1efc5b81c5120bf61d0a39ade
Kelvin Mathew <kelvinmathew2004@gmail.com>
2025-06-11 18:36:32 -0500

    Step 4: Handle Upgrades

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,27 @@ import samosa from './assets/samosa.png';
 const App = () => {
 
   const [count, setCount] = useState(0);
-  const [multiplier] = useState(1);
+  const [multiplier, setMultiplier] = useState(1);
 
   const updateCount = () => setCount(count + multiplier);
+  
+  const buyDoubleStuffed = () => {
+    if (count >= 10) {
+      setMultiplier(multiplier * 2);
+    }
+  }
+
+  const buyPartyPack = () => {
+    if (count >= 100) {
+      setMultiplier(multiplier * 5);
+    }
+  }
+
+  const buyFullFeast = () => {
+    if (count >= 100) {
+      setMultiplier(multiplier * 10);
+    }
+  }
 
   return (
     <div className="App">
@@ -19,17 +37,17 @@ const App = () => {
           <div className="upgrade">
             <h3>Double Stuffed 👯‍♀️</h3>
             <p>2x per click</p>
-            <button>10 samosas</button>
+            <button onClick={buyDoubleStuffed} >10 samosas</button>
           </div>
           <div className="upgrade">
             <h3>Party Pack 🎉</h3>
             <p>5x per click</p>
-            <button>100 samosas</button>
+            <button onClick={buyPartyPack}>100 samosas</button>
           </div>
           <div className="upgrade">
-            <h3>Full Feast 👨‍🍳</h3>
+            <h3>Full Feast 👩🏽‍🍳</h3>
             <p>10x per click</p>
-            <button>1000 samosas</button>
+            <button onClick={buyFullFeast}>1000 samosas</button>
           </div>
         </div>
       </div>
